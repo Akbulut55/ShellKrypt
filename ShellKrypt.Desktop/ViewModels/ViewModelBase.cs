6e006976ac09ffe8a40619bcce29fc0014872df6
Akbulut55 <karbarkurlas@gmail.com>
2026-02-08 23:40:22 +0300

    Add project files.

---
+++ ShellKrypt.Desktop/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ShellKrypt.Desktop.ViewModels
+{
+    public abstract class ViewModelBase : ObservableObject
+    {
+    }
+}
---
+++ ShellKrypt.Desktop/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ShellKrypt.Desktop.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
