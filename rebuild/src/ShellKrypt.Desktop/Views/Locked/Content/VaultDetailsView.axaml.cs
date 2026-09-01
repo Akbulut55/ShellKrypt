9a94e91b50040bbd501a12383933f045c37e692d
Akbulut55 <karbarkurlas@gmail.com>
2026-09-02 01:03:54 +0300

    delete rebuild

--- rebuild/src/ShellKrypt.Desktop/Views/Locked/Content/VaultDetailsView.axaml.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls;
-
-namespace ShellKrypt.Desktop.Views.Locked.Content;
-
-public partial class VaultDetailsView : UserControl
-{
-    public VaultDetailsView()
-        => InitializeComponent();
-}
--- rebuild/src/ShellKrypt.Desktop/Views/Locked/Content/WelcomeView.axaml.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls;
-
-namespace ShellKrypt.Desktop.Views.Locked.Content;
-
-public partial class WelcomeView : UserControl
-{
-    public WelcomeView()
-        => InitializeComponent();
-}
